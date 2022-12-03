d78542f851176091a4fad495ba073073c43628fe
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-03 08:06:46 +0200

    d3p2

--- day3.py
+++ day3.py
@@ -1,30 +1,41 @@
-# def getPriority(x):
-#     return ord(x)
-
 
 f = open("day3.txt")
 result = ""
 for x in f:
-    chars = list(x)
-    sack1, sack2 = chars[:len(chars)//2], chars[len(chars)//2:]
-    share =  list(set(sack1).intersection(sack2))
-    result = result + share[0]
-    #result.append(share)
+    in1 = x.rstrip()
+    in2 = f.readline().rstrip()
+    in3 = f.readline().rstrip()
+    cm = ''.join(set(in1).intersection(in2).intersection(in3))
+    result = result + cm
 
+print(result)
 res = 0
 for x in result:
     if ord(x) < 91:
-        res = res + ord(x) -38
+        res = res + ord(x) - 38
         
     else:
         res = res + ord(x) - 96
-    # txt = str(x) +":" + str(res)
-    # print(txt)
 
 print(res)
-# priorities = 0
-# print(ord('a'))
+
+
+
+#part 1
+#f = open("day3.txt")
+# result = ""
+# for x in f:
+#     chars = list(x)
+#     sack1, sack2 = chars[:len(chars)//2], chars[len(chars)//2:]
+#     share =  list(set(sack1).intersection(sack2))
+#     result = result + share[0]
+
+# res = 0
 # for x in result:
-#     priorities = priorities + ord(chr(x))
-#     txt =x + ": " + ord(chr(x))
-#     print(txt)
+#     if ord(x) < 91:
+#         res = res + ord(x) -38
+        
+#     else:
+#         res = res + ord(x) - 96
+
+# print(res)
