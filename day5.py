f334bc3a3e99cc55bb10e5a0890c2151922016e9
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-05 11:19:17 +0200

    d5 done

--- day5.py
+++ day5.py
@@ -21,37 +21,54 @@ def GetStartStacks():
         stacks.append(res)
     return stacks
 
-
-def getStackLen(index):
+def getStackLen(stacks,index):
     for i in range(0,len(stacks)):
         if stacks[i][index-1] == '0': 
             return i -1
     return len(stacks)-1
 
-def addNewLine(end, val):
+def addNewLine(stacks,end, val):
     line = []
     for x in range(0, len(stacks[0])):
         line.append("0")
     line[end-1] = val
     stacks.append(line)
 
-def solvePart2(stacks):
-
+def solveStack(part):
+    stacks = GetStartStacks()
     input = open("day5.txt")
     for x in input:
         if not "move" in x: continue
         x = x.split(" ")
         move, str,end ,  = int(x[1]), int(x[3]), int(x[5])
-        strCnt = getStackLen(str)
-        endCnt = getStackLen(end)
-        for i in range(0,move):
-            val =stacks[strCnt-move+i+1][str-1]
-            stacks[strCnt-move+i+1][str-1] = "0"
-            if endCnt+i == len(stacks) -1:
-                addNewLine(end, val)
-            else:
-                stacks[endCnt+1+i][end-1] = val
+        strCnt = getStackLen(stacks,str)
+        endCnt = getStackLen(stacks, end)
+        if part == "part2":
+            solveP2(stacks, move, str, end, strCnt, endCnt)
+        elif part == "part1":
+            solveP1(stacks, move, str, end, strCnt, endCnt)
     return stacks
+
+def solveP2(stacks, move, str, end, strCnt, endCnt):
+    for i in range(0,move):
+        val =stacks[strCnt-move+i+1][str-1]
+        stacks[strCnt-move+i+1][str-1] = "0"
+        if endCnt+i == len(stacks) -1:
+            addNewLine(stacks, end, val)
+        else:
+            stacks[endCnt+1+i][end-1] = val
+
+def solveP1(stacks, move, str, end, strCnt, endCnt):
+    for i in range(0,move):
+        val = stacks[strCnt][str-1]
+        stacks[strCnt][str-1] = "0"
+        strCnt = strCnt -1
+        if endCnt == len(stacks) -1:
+            addNewLine(stacks, end, val)
+        else:
+            stacks[endCnt+1][end-1] = val
+        endCnt = endCnt +1
+
 
 def getResult(stacks):
     res = ""
@@ -63,10 +80,5 @@ def getResult(stacks):
                 break 
     return res
 
-stacks = GetStartStacks()
-stacks = solvePart2(stacks)
-result = getResult(stacks)
-print(result)
-# print("Result:")
-# for x in stacks:
-#     print(x)
+print(getResult(solveStack("part1")))
+print(getResult(solveStack("part2")))
