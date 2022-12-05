0d70818d32af69b8a1a781979a9d76748ffb4e0f
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-05 11:03:34 +0200

    part2

--- day5.py
+++ day5.py
@@ -1,27 +1,27 @@
-f = open("day5.txt")
-slines = []
-for x in f:
-    if x == '\n': break
-    x = " " + x
-    slines.append(x.strip('\n'))
-slines.reverse()
-first = slines[0].strip()
-first = ' '.join(first.split())
-first = first.split(" ")
-stacks = []
-#del slines[0]
-K = "_"
-N = 4
-for x in slines:
-    res = ''.join(ele if idx % N or idx == 0 else K for idx, ele in enumerate(x))
-    res = res[1:]
-    res = res.replace("   ", "[0]")
-    res = res.replace("[","").replace("]","").replace(" ", "")
-    res = res.split("_")
-    stacks.append(res)
+def GetStartStacks():
+    f = open("day5.txt")
+    slines = []
+    for x in f:
+        if x == '\n': break
+        x = " " + x
+        slines.append(x.strip('\n'))
+    slines.reverse()
+    first = slines[0].strip()
+    first = ' '.join(first.split())
+    first = first.split(" ")
+    stacks = []
+    K = "_"
+    N = 4
+    for x in slines:
+        res = ''.join(ele if idx % N or idx == 0 else K for idx, ele in enumerate(x))
+        res = res[1:]
+        res = res.replace("   ", "[0]")
+        res = res.replace("[","").replace("]","").replace(" ", "")
+        res = res.split("_")
+        stacks.append(res)
+    return stacks
 
-for x in stacks:
-    print(x)
+
 def getStackLen(index):
     for i in range(0,len(stacks)):
         if stacks[i][index-1] == '0': 
@@ -34,30 +34,39 @@ def addNewLine(end, val):
         line.append("0")
     line[end-1] = val
     stacks.append(line)
-input = open("day5.txt")
-#input = ["move 4 from 1 to 9"]
 
-#input = ["move 3 from 3 to 7"]
-#input = ["move 1 from 2 to 1", "move 3 from 1 to 3"]
-#input = ["move 3 from 3 to 7", "move 4 from 1 to 9"]
-for x in input:
-    if not "move" in x: continue
-    x = x.split(" ")
-    move, str,end ,  = int(x[1]), int(x[3]), int(x[5])
-    strCnt = getStackLen(str)
-    endCnt = getStackLen(end)
-    for i in range(0,move):
-        val = stacks[strCnt][str-1]
-        stacks[strCnt][str-1] = "0"
-        strCnt = strCnt -1
-        if endCnt == len(stacks) -1:
-            addNewLine(end, val)
-        else:
-            stacks[endCnt+1][end-1] = val
-        endCnt = endCnt +1
-    # txt = "startStack has {}, end stack has {} crates"
-    # print(txt.format(strCnt,endCnt))
+def solvePart2(stacks):
 
-print("Result:")
-for x in stacks:
-    print(x)
+    input = open("day5.txt")
+    for x in input:
+        if not "move" in x: continue
+        x = x.split(" ")
+        move, str,end ,  = int(x[1]), int(x[3]), int(x[5])
+        strCnt = getStackLen(str)
+        endCnt = getStackLen(end)
+        for i in range(0,move):
+            val =stacks[strCnt-move+i+1][str-1]
+            stacks[strCnt-move+i+1][str-1] = "0"
+            if endCnt+i == len(stacks) -1:
+                addNewLine(end, val)
+            else:
+                stacks[endCnt+1+i][end-1] = val
+    return stacks
+
+def getResult(stacks):
+    res = ""
+    for i in range(0, len(stacks[0])):
+        for j in range(0, len(stacks)):
+            if j+1 == len(stacks): return 0
+            if stacks[j+1][i] == "0": 
+                res = res + stacks[j][i] 
+                break 
+    return res
+
+stacks = GetStartStacks()
+stacks = solvePart2(stacks)
+result = getResult(stacks)
+print(result)
+# print("Result:")
+# for x in stacks:
+#     print(x)
