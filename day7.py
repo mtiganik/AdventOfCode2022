82659e3d5cbb4a10f7f101903b1b300697afe5e0
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-07 16:05:31 +0200

    refactor d7

--- day7.py
+++ day7.py
@@ -1,8 +1,41 @@
+def addFolderSizes(folder):
+    for y in folder:
+        if type(folder[y]) is int and "size" not in y:
+            folder["size"] = folder["size"] + folder[y]
+        if type(folder[y]) is dict:
+            newFolder = folder[y]
+            newFolder["size"] = 0
+            folder["size"] = folder["size"] + addFolderSizes(newFolder)
+    return folder["size"]
+
+
+def findfolderToDelete(folder):
+    global part2Result
+    for y in folder:
+        if "size" in y:
+            if folder[y] > needed and folder[y] < part2Result:
+                part2Result = folder[y]
+        if type(folder[y]) is dict:
+            newFolder = folder[y]
+            findfolderToDelete(newFolder)
+
+def folderSizeMeas(folder):
+    global part1result
+    for y in folder:
+        if "size" in y:
+            if folder[y] <= 100000:
+                part1result = part1result + folder[y]
+        if type(folder[y]) is dict:
+            newFolder = folder[y]
+            folderSizeMeas(newFolder)
+
 input = open("day7.txt")
 f = {"main": {}, "size": 0}
 input.readline()
 input.readline()
 currFolder = "main"
+
+#Get Data Object:
 for x in input:
     if x.startswith("$ cd"):
         if ".." in x:
@@ -27,50 +60,20 @@ for x in input:
         element[key] = value
 
 
-def folderSearch(folder):
-    for y in folder:
-        if type(folder[y]) is int and "size" not in y:
-            folder["size"] = folder["size"] + folder[y]
-        if type(folder[y]) is dict:
-            newFolder = folder[y]
-            newFolder["size"] = 0
-            folder["size"] = folder["size"] + folderSearch(newFolder)
-    return folder["size"]
-main = f["main"]
 
-folderSearch(f)
+addFolderSizes(f)
+
+#Part1:
+part1result = 0
+folderSizeMeas(f)
+print("Part1: ", part1result)
+
+#Part2:
 used = f["size"]
 total = 70000000
 available = total - used
 needed = 30000000 - available
-print("needed: ", needed)
-
 part2Result = 30000000
-def findfolderToDelete(folder):
-    global part2Result
-    for y in folder:
-        if "size" in y:
-            if folder[y] > needed and folder[y] < part2Result:
-                part2Result = folder[y]
-        if type(folder[y]) is dict:
-            newFolder = folder[y]
-            findfolderToDelete(newFolder)
-
-part1result = 0
-def folderSizeMeas(folder):
-    global part1result
-    for y in folder:
-        if "size" in y:
-            if folder[y] <= 100000:
-                print(folder[y])
-                result = result + folder[y]
-        if type(folder[y]) is dict:
-            newFolder = folder[y]
-            folderSizeMeas(newFolder)
-            
-folderSizeMeas(f)
-print(part1result)
-
 findfolderToDelete(f)       
-print(part2Result)
+print("Part2: ", part2Result)
 
