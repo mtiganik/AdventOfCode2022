e549df200c3da874a427a6cac3d62da8b9775ac0
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-07 15:37:24 +0200

    d7 p1

--- day7.py
+++ day7.py
@@ -1,56 +1,68 @@
 input = open("day7.txt")
-f = {"main": {}}
+f = {"main": {}, "size": 0}
 input.readline()
 input.readline()
 currFolder = "main"
 for x in input:
-    # if x[0] == '$':
-    #     print("New cmd")
     if x.startswith("$ cd"):
         if ".." in x:
             path = currFolder.split("/")
             path.pop()
             currFolder = '/'.join(path)
-            print("folder back: ", currFolder )
         else:
             newFolder = x.split()[2]
             currFolder = currFolder + "/" + newFolder
-            print("new folder: ", currFolder)
-    if x.startswith("$ ls"):
-        print("list items")
-    else:
+    # elif x.startswith("$ ls"):
+    #     print("list items")
+    elif "$ ls" not in x:
         path = currFolder.split("/")
-        prevElement = {}
         element = {}
-        for p in path:
-            prevElement = element
-            element = f[p]  
+        key, value = "", ""
         if "dir" in x:
-            x = x.replace("dir ", "").strip()
-            f[x] = {}
+            key, value = x.replace("dir ", "").strip(), {}
         if x[0].isdigit():
             file = x.split()
-            f[file[1]] = int(file[0])
+            key, value = file[1], int(file[0])
+        element = f
+        for y in path:
+            element = element[y]
+        element[key] = value
 
 
-#print(f)
 
-f = {"main" : {
-"a":{"e": {"i": 584}, "f":29116, "g": 2557, "h.lst": 62596},
-"b.txt":14848514, 
-"c.dat": 8504156, 
-"d":{"j":4060174,"d.log": 8033020,"d.ext":5626152,"k": 7214296, } }}
+# print(f)
 
-print(f["main"]["a"]["e"]["i"])
+# f = {"main" : {
+# "a":{"e": {"i": 584}, "f":29116, "g": 2557, "h.lst": 62596},
+# "b.txt":14848514, 
+# "c.dat": 8504156, 
+# "d":{"j":4060174,"d.log": 8033020,"d.ext":5626152,"k": 7214296, } }}
+
 
 def folderSearch(folder):
     for y in folder:
-        if type(folder[y]) is int:
-                #BL here
-            print(folder[y])
+        if type(folder[y]) is int and "size" not in y:
+            folder["size"] = folder["size"] + folder[y]
         if type(folder[y]) is dict:
             newFolder = folder[y]
-            folderSearch(newFolder)
+            newFolder["size"] = 0
+            folder["size"] = folder["size"] + folderSearch(newFolder)
+    return folder["size"]
 main = f["main"]
 
-folderSearch(main)
+folderSearch(f)
+
+result = 0
+def folderSizeMeas(folder):
+    global result
+    for y in folder:
+        if "size" in y:
+            if folder[y] <= 100000:
+                print(folder[y])
+                result = result + folder[y]
+        if type(folder[y]) is dict:
+            newFolder = folder[y]
+            folderSizeMeas(newFolder)
+            
+folderSizeMeas(f)
+print(result)
