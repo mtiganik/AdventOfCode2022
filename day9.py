d155cff7616d1785a2fc4b373d811a2c4e75a477
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-09 11:46:31 +0200

    debugging

--- day9.py
+++ day9.py
@@ -26,90 +26,104 @@ tail = [["#"]]
 
 f = open("day9.txt")
 #f = [ "D 2", "L 3", "D 1"]
-ih,jh, it,jt = 0,0,0,0
-
+ik = [0,0,0,0,0,0,0,0,0,0]
+jk = [0,0,0,0,0,0,0,0,0,0]
 
 for x in f:
     x = x.split(" ",1)
     dirc, steps = x[0], int(x[1])
     for k in range(0,steps):
+        #first knot:
         if dirc == "R":
-            if jh == len(grid[0])-1:
-                addRight(ih,grid,"H")
+            if jk[0] == len(grid[0])-1:
+                addRight(ik[0],grid,"H")
                 addRight(0,tail,".")
             else:
-                grid[ih][jh+1] = "H"
-            grid[ih][jh] = "."
-            jh = jh +1
+                grid[ik[0]][jk[0]+1] = "H"
+            grid[ik[0]][jk[0]] = "."
+            jk[0] = jk[0] +1
         if dirc == "L":
-            if jh == 0:
-                addLeft(ih,grid,"H")
+            if jk[0] == 0:
+                addLeft(ik[0],grid,"H")
                 addLeft(0,tail,".")
-                grid[ih][jh+1] = "."
-                jt = jt +1
+                grid[ik[0]][jk[0]+1] = "."
+                #jk[1] = jk[1] +1
+                jk = [y+1 for y in jk]
+                jk[0] = 0
             else:
-                grid[ih][jh-1] = "H"
-                grid[ih][jh] = "."
-                jh = jh - 1
+                grid[ik[0]][jk[0]-1] = "H"
+                grid[ik[0]][jk[0]] = "."
+                jk[0] = jk[0] - 1
         if dirc == "U":
-            if ih == 0:
-                addUp(jh,grid,"H")
+            if ik[0] == 0:
+                addUp(jk[0],grid,"H")
                 addUp(0,tail,".")
-                grid[ih+1][jh] = "."
-                it = it +1
+                grid[ik[0]+1][jk[0]] = "."
+                #ik[1] = ik[1] +1
+                ik = [y+1 for y in ik]
+                ik[0]=0
             else:
-                grid[ih-1][jh] = "H"
-                grid[ih][jh] = "."
-                ih = ih-1
+                grid[ik[0]-1][jk[0]] = "H"
+                grid[ik[0]][jk[0]] = "."
+                ik[0] = ik[0]-1
         if dirc == "D":
-            if ih == len(grid) -1:
-                addDown(jh,grid,"H")
+            if ik[0] == len(grid) -1:
+                addDown(jk[0],grid,"H")
                 addDown(0,tail,".")
             else:
-                grid[ih+1][jh]="H"
-            grid[ih][jh] = "."
-            ih = ih+1 
+                grid[ik[0]+1][jk[0]]="H"
+            grid[ik[0]][jk[0]] = "."
+            ik[0] = ik[0]+1 
         
         ## Tail Setup
-        #Right
-        if jh - jt == 2:
-            grid[it][jt] = '.'
-            it = ih
-            jt = jt+1
-            grid[it][jt] = 'T'
-            tail[it][jt] = "#"
-        #Down
-        elif ih-it == 2:
-            grid[it][jt] = "."
-            jt=jh
-            it = it+1
-            grid[it][jt] = "T"
-            tail[it][jt] = "#"
-        #Left
-        elif jh - jt == -2:
-            grid[it][jt] ="."
-            it = ih
-            jt = jt-1
-            grid[it][jt] = "T"
-            tail[it][jt] = "#"
-        #Up
-        elif ih - it == -2:
-            grid[it][jt] = "."
-            jt = jh
-            it = it-1
-            grid[it][jt] = "T"
-            tail[it][jt] = "#"
-
+        for i in range(1,len(ik)):
+            #Right
+            if jk[i-1] - jk[i] == 2:
+                grid[ik[i]][jk[i]] = '.'
+                ik[i] = ik[i-1]
+                jk[i] = jk[i]+1
+                grid[ik[i]][jk[i]] = "% s" % i
+                #tail[it][jt] = "#"
+            #Down
+            elif ik[i-1]-ik[i] == 2:
+                grid[ik[i]][jk[i]] = "."
+                jk[i]=jk[i-1]
+                ik[i] = ik[i]+1
+                grid[ik[i]][jk[i]] = "% s" % i
+                #tail[it][jt] = "#"
+            #Left
+            elif jk[i-1] - jk[i] == -2:
+                grid[ik[i]][jk[i]] ="."
+                ik[i] = ik[i-1]
+                jk[i] = jk[i]-1
+                grid[ik[i]][jk[i]] = "% s" % i
+                #tail[it][jt] = "#"
+            #Up
+            elif ik[i-1] - ik[i] == -2:
+                grid[ik[i]][jk[i]] = "."
+                jk[i] = jk[i-1]
+                ik[i] = ik[i]-1
+                grid[ik[i]][jk[i]] = "% s" % i
+                #tail[it][jt] = "#"
+    
         # print("Round")
         # for x in grid:
         #     print(x)
-    # print(steps)
-    #if "R" in x:
-for x in tail:
-    str = ""
-    str.join(x)
-    print(x)
-cnt = 0
-for x in tail:
-    cnt = cnt + x.count("#")
-print(cnt)
+    print("Round")
+    for x in grid:
+        str = ""
+        str = str.join(x)
+        print(str)
+
+# str = ""
+# list1 = ["a","b","c"]
+# str.join(list1)
+# print()
+# for x in tail:
+#     str = ""
+#     str = str.join(x)
+#     print(str)
+# cnt = 0
+# for x in tail:
+#     cnt = cnt + x.count("#")
+# print(cnt)
