ffd4cd493e1b90ef1e5cb20600535f1683a6b0ba
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-10 09:24:35 +0200

    part2

--- day10.py
+++ day10.py
@@ -1,31 +1,56 @@
-def checkCycle(cycle):
-    if cycle in impCycles:
 
-        cycleval = cycle* X
-        impCycleVals.append(cycleval)
-        # txt = "cycle occured, val is {}, X is {}"
-        # print(txt.format(cycleval, X))
+def writeGrid():
+    global spritePos, outGrid, cycle, currRow
+    if spritePos[cycle -1] == "#":
+        outGrid[currRow].append("#")
+    else:
+        outGrid[currRow].append(".")
+
+def changeSprite(spritePos, X):
+    newSprite = ["."]*41
+    newSprite[X-1] = "#"
+    newSprite[X] = "#"
+    newSprite[X+1] = "#"
+    return newSprite
 
 f = open("day10.txt")
-impCycles = [20,60,100,140,180,220]
-impCycleVals=[]
-#f = ["noop", "addx 3", "addx -5"]
 
+spritePos = "###....................................."
+spritePos = list(spritePos)
+
+outGrid = [[]]
+currRow = 0
 cycle = 1
 X = 1
 for x in f:
     if "noop" in x:
+        writeGrid()
         cycle = cycle +1
-        checkCycle(cycle)
     else:
+        writeGrid()
         cycle = cycle +1
-        checkCycle(cycle)
+        if (cycle-1)%40 == 0:
+            print("cycle changed at ", x)
+            cycle = 1
+            currRow = currRow +1
+            outGrid.append([])
+
+        writeGrid()
         val = int(x.split()[1])
         X = X + val
+        spritePos = changeSprite(spritePos, X)
         cycle = cycle +1
-        checkCycle(cycle)
+    if (cycle-1)%40 == 0:
+        print("cycle changed at ", x)
+        cycle = 1
+        currRow = currRow +1
+        outGrid.append([])
+
 
 sum = 0
-for x in impCycleVals:
-    sum = sum + x
-print("Sum is: ", sum)
+
+
+for x in outGrid:
+    str = ""
+    str = str.join(x)
+    print(str)
