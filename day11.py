e4ed4f091f0345ee83b6957ec23e7ddce5ce6cc2
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-11 09:04:05 +0200

    p2 debugg

--- day11.py
+++ day11.py
@@ -1,15 +1,16 @@
-def getWorryLev(item, Operation):
+import math
+def getWorryLev(item, Operation, divideBy):
     returnVal = item
     if Operation.count("old") == 2 and "*" in Operation:
         returnVal = returnVal * returnVal
-        return int(returnVal/3)
+        return int(returnVal/divideBy)
     ls=Operation.split()
     num = int(ls[2])
     if "*" in Operation:
         returnVal = returnVal * num
     elif "+" in Operation:
         returnVal = returnVal + num
-    return int(returnVal / 3)
+    return int(returnVal / divideBy)
 data= []
 
 f = open("day11.txt")
@@ -32,35 +33,39 @@ for x in f:
     ifFalse = int(nl.split()[5])
     f.readline()
     Do = {"Monkey": monkey, "items": items, "Operation": operation, 
-    "DivisibleBy": divisibleBy, "ifTrue": ifTrue, "ifFalse":ifFalse, "inspectionCnt": 0}
+    "DivisibleBy": divisibleBy, "ifTrue": ifTrue, "ifFalse":ifFalse, "inspectionCnt": 0,
+    "ifFalseCnt":0}
     data.append(Do)
 
-# for x in data:
-#     print(x)
-#Data object:
-# data = [{"Monkey":0, "items":[79,98], "Operation":"old * 19", 
-# "DivisibleBy": 23, "ifTrue": 2, "ifFalse":3, "inspectionCnt": 0},
-# {"Monkey":1, "items":[54,65,75,74], "Operation":"old + 6", 
-# "DivisibleBy": 19, "ifTrue": 2, "ifFalse":0, "inspectionCnt": 0},
-# {"Monkey":2, "items":[79,60,97], "Operation":"old * old", 
-# "DivisibleBy": 13, "ifTrue": 1, "ifFalse":3, "inspectionCnt": 0},
-# {"Monkey":3, "items":[74], "Operation":"old + 3", 
-# "DivisibleBy": 17, "ifTrue": 0, "ifFalse":1, "inspectionCnt": 0},]
+#Part 1
+# cycles = 20
+# divideBy = 3
 
+#Part 2
+cycles = 20
+divideBy = 1
 
 dummy = 0
-for i in range(0,20):
+for i in range(0,cycles):
     dummy = i
     for x in data:
         monkeyNo = x["Monkey"]
         items = x["items"]
         Operation = x["Operation"]
         for k in items:
-            newLevel = getWorryLev(k, Operation)
+            newLevel = getWorryLev(k, Operation, divideBy)
             if (newLevel/x["DivisibleBy"]).is_integer():
+                newLevel = newLevel/x["DivisibleBy"]
                 newMonkeyNo = x["ifTrue"]
             else:
                 newMonkeyNo = x["ifFalse"]
+                if Operation.count("old") == 2 and "*" in Operation:
+                    newLevel = math.sqrt(newLevel)
+                elif "*" in Operation:
+                    ls=Operation.split()
+                    num = int(ls[2])
+
+                #x["ifFalseCnt"] = x["ifFalseCnt"] +1
             x["inspectionCnt"] = x["inspectionCnt"] +1
             newMonkey = data[newMonkeyNo]
             newMonkey["items"].append(newLevel)
