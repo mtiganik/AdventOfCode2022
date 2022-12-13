4a558588e0065289f19b0d8640e72c96d9fbb411
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-13 07:51:10 +0200

    d12

--- day12.py
+++ day12.py
@@ -1,135 +1,40 @@
-f = open("day12.txt")
+#f = open("day12.txt")
+from collections import deque
 
-grid = []
-for x in f:
-    x = x.strip()
-    x = list(x)
-    grid.append(x)
+# grid = []
+# for x in f:
+#     x = x.strip()
+#     x = list(x)
+#     grid.append(x)
 
-def containsAll(str, set):
-    """ Check whether sequence str contains ALL of the items in set. """
-    return 0 not in [c in str for c in set]
 
-def upDownEnd(str):
-    if "v" in str:
-        if "^" in str: 
-            return True
-    if "<" in str:
-        if ">" in str: 
-            return True
-    return False
+grid = [list(x) for x in open("day12.txt").read().strip().splitlines()]
 
-def shouldGo(str, strlst, p,p1):
-    for x in strlst:
-        if x == str + p:
-            return False
-        if x == str + p1:
-            return False
+for r, row in enumerate(grid):
+    for c, item in enumerate(row):
+        if item == "S":
+            grid[r][c] = "a"
+        if item == "E":
+            er = r
+            ec = c
+            grid[r][c] = "z"
 
-    return True
+q = deque()
+q.append((0, er, ec))
 
-# if[ele for ele in strlst if(ele[:-1] in str1)]:
-#     print("str is in list, ", str1)
-#path = [['.']*len(grid[0]) for _ in range(len(grid))]
-sX,sY,destX,destY=0,0,0,0
-for x in grid:
-    if "S" in x:
-        sX = x.index("S")
-        sY = grid.index(x)
-    if "E" in x:
-        destX = x.index("E")
-        destY = grid.index(x)
+vis = {(er, ec)}
 
-currChar = 'd'
-move = 0
-atmt = 0
-wasMovement = True
-path = ""
-paths = [""]
-validPaths = [""]
-X,Y = sX,sY
-for i in range(0,20):
-    atmt = i
-    while (X,Y) != (destX,destY):
-        wasMovement = False
-        #check down
-        if Y != len(grid)-1:
-            if shouldGo(path,paths,"vX","vD"):
-                if ord(grid[Y+1][X]) == ord(currChar) +1 or (grid[Y+1][X] == 'E' and currChar=='z'):
-                    path = path + "v"
-                    Y = Y+1
-                    currChar = grid[Y][X]  
-                    wasMovement = True
-                    continue
-                elif ord(grid[Y+1][X]) == ord(currChar):
-                    path = path + "v"
-                    Y = Y+1
-                    wasMovement = True
-                    continue
-        #check right
-        if X != len(grid[0]) -1:
-            if shouldGo(path,paths,">X",">D"):
-                if ord(grid[Y][X+1]) == ord(currChar) +1 or (grid[Y][X+1] == 'E' and currChar=='z'):
-                    path = path + ">"
-                    X = X + 1
-                    currChar = grid[Y][X] 
-                    wasMovement = True
-                    continue
-                elif ord(grid[Y][X+1]) == ord(currChar):
-                    path = path + ">"
-                    wasMovement = True
-                    X = X + 1
-        #check top
-        if Y != 0:
-            if shouldGo(path,paths,"^X","^D"):
-                if ord(grid[Y-1][X]) == ord(currChar) +1 or (grid[Y-1][X] == 'E' and currChar=='z'):
-                    path = path + "^"
-                    Y = Y - 1
-                    currChar = grid[Y][X]
-                    wasMovement = True
-                    continue
-                elif ord(grid[Y-1][X]) == ord(currChar):
-                    path = path + "^"
-                    wasMovement = True
-                    Y = Y - 1
-        #Check left
-        if X != 0:
-            if shouldGo(path,paths,"<X", "<D"):
-                if ord(grid[Y][X-1]) == ord(currChar) +1 or (grid[Y][X-1] == 'E' and currChar=='z'):
-                    path = path + "<"
-                    X = X - 1
-                    currChar = grid[Y][X]
-                    wasMovement = True
-                    continue
-                elif ord(grid[Y][X-1]) == ord(currChar):
-                    path = path + "<"
-                    X = X - 1
-                    wasMovement = True
-
-        #Check dead end    
-
-        if containsAll(path[-4:], "^<v>") or upDownEnd(path[-2:]) or wasMovement == False:
-            #print("Dead end reached on path: ", path)
-            if containsAll(path[-4:], "^<v>"): path = path[:-4]
-            if path == "": 
-                break
-            path = path + "X"
-            paths.append(path) 
-            # atmt = atmt +1
-            path= ""
-            X,Y,currChar = sX,sY,'d'
-    else:
-        print("reach destination!!")
-        path = path + "D"
-        paths.append(path) 
-        validPaths.append(path)
-        path= ""
-        X,Y,currChar = sX,sY,'d'
-        #print(path)
-def lenVal(e):
-    return len(e)
-validPaths.sort(key=lenVal)
-for x in validPaths:
-    print(x, len(x)-1)
-print(path)
-print(X,Y,destX,destY)
+while q:
+    d, r, c = q.popleft()
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]):
+            continue
+        if (nr, nc) in vis:
+            continue
+        if ord(grid[nr][nc]) - ord(grid[r][c]) < -1:
+            continue
+        if grid[nr][nc] == "a":
+            print(d + 1)
+            exit(0)
+        vis.add((nr, nc))
+        q.append((d + 1, nr, nc))
