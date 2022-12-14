a666f3184ccdc32d47407010047f6339fd55965c
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-14 09:38:47 +0200

    p1 done

--- day14.py
+++ day14.py
@@ -1,6 +1,6 @@
 
 rangey = 167 # if excpection put it bigger
-rangey = 12 #test
+# rangey = 12 #test
 grid = [["."]*1000 for _ in range(rangey)]
 
 f = open("day14.txt")
@@ -14,43 +14,46 @@ for x in f:
         p2ds.append(p)
     sand.append(p2ds)
 grid[0][500] = "+"
-
 for x in sand:
     for k in range(0, len(x)-1):
         point = x[k]
         stepx = 1 if x[k][0] < x[k+1][0] else -1
         stepy = 1 if x[k][1] < x[k+1][1] else -1
-        for i in range(x[k][0],x[k+1][0], stepx):
+        for i in range(x[k][0],x[k+1][0] + stepx, stepx):
             grid[point[1]][i] = "#"
-        for j in range(x[k][1], x[k+1][1], stepy):
+        for j in range(x[k][1], x[k+1][1] + stepy, stepy):
             grid[j][point[0]] = "#"
 
-for x in grid:
-    str = "".join(x[490:510:1]) #test
-    print(str)
 
 
-#Falling down
-#findSpot to fall
 def findSpotToFall(y,x):
-    #straight down
-    while(1):
+    stp = 1
+    while(y < len(grid)-1):
         if(grid[y+1][x]) == ".":
             y = y+1
         else:
             if grid[y+1][x-1]==".":
                 findSpotToFall(y+1,x-1)
-                # y = y+1
-                # x = x-1
             elif grid[y+1][x+1] == ".":
                 findSpotToFall(y+1,x+1)
-                # y = y+1
-                # x = x+1
             else:
                 grid[y][x] = "o"
-                print("reached down at: ", x,y)
             break
 
-#str = "".join(x[480:540:1]) #data
-#for i in range(0,22):
-    #findSpotToFall(0,500)
+
+checker = 1
+cycle = 0
+
+for i in range(0,1000):
+    cycle = cycle +1
+    findSpotToFall(0,500)
+
+for x in grid:
+    str = "".join(x[420:560:1]) #test
+    # str = "".join(x[490:510:1]) #test
+    print(str)
+cnt = 0
+for x in grid:
+    cnt = cnt + x.count("o")
+
+print(cnt)
