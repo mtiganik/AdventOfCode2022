30d96105a34bc780a793c243a32373bf1ff7f025
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-18 12:38:37 +0200

    p2 debug

--- day18/day18.py
+++ day18/day18.py
@@ -4,8 +4,6 @@ data = []
 for k in f:
     k = k.rstrip().split(",")
     x,y,z = int(k[0]), int(k[1]), int(k[2])
-    sArea = 6
-    #item = {"x":x,"y":y,"z":z,"A":6}
     item = [x,y,z]
     data.append(item)
 
@@ -16,9 +14,19 @@ def checkCollision(i,j):
         return True
     return False
 
-for i in range(0,len(data)-1):
-    for j in range(i,len(data)):
-        if checkCollision(data[i],data[j]):
-            sum = sum -2
+# for i in range(0,len(data)-1):
+#     for j in range(i,len(data)):
+#         if checkCollision(data[i],data[j]):
+#             sum = sum -2
+sum = 4418
+# print("Part 1:", sum)
+
+
+for i in range(1,20):
+    for j in range(1,20):
+        for k in range(1,20):
+            if [i,j,k] in data: continue
+            elif [i-1,j,k] in data and [i+1,j,k] in data and [i,j-1,k] in data and [i,j+1,k] in data and [i,j,k-1] in data and [i,j,k+1] in data:
+                sum = sum-6
 
 print(sum)
