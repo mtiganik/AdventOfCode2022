1ea7e7fb4449908eea5d676bbf4806b02ac0b0c5
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-20 12:27:31 +0200

    part2

--- day20/day20.py
+++ day20/day20.py
@@ -2,11 +2,18 @@ f = open("day20/day20.txt")
 vals = []
 lst = []
 cnt = 0
-for x in f:
-    vals.append((cnt,int(x.strip())))
-    lst.append((cnt,int(x.strip())))
-    cnt = cnt+1
 
+#part1
+magicNumber = 1
+iterations = 1
+#part2:
+magicNumber = 811589153
+iterations = 10
+
+for x in f:
+    vals.append((cnt,int(x.strip())*magicNumber))
+    lst.append((cnt,int(x.strip())*magicNumber))
+    cnt = cnt+1
 size = len(vals)
 
 def mix(vals, lst):
@@ -16,12 +23,11 @@ def mix(vals, lst):
         del lst[oi]
         lst.insert(ni if ni else len(lst), n)
 
-mix(vals,lst)
-zp = [
-    idx for idx, tup in enumerate(lst) if tup[1] == 0
-][0]
+for i in range(0,iterations):
+    mix(vals,lst)
+    
+zp = [idx for idx, tup in enumerate(lst) if tup[1] == 0][0]
 
 el1, el2,el3 = lst[(zp+1000)%size][1], lst[(zp+2000)%size][1], lst[(zp+3000)%size][1]
-print(el1,el2,el3)
 
 print("Part1:", el1+el2+el3)
