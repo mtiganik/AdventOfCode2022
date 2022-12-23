9ef8c14504176acbd62fdad175b93eadfe308605
Mihkel Tiganik <mtiganik@gmail.com>
2022-12-23 11:05:09 +0200

    day23 part 1 done

--- day23/day23.py
+++ day23/day23.py
@@ -18,24 +18,23 @@ def checkAvailabilityInDir(elve, dirToCheck):
         if grid[y-1][x-1] == "." and grid[y][x-1] == "." and grid[y+1][x-1] == ".": return True
         else: return False
     if dirToCheck == 3:
-        if grid[y-1][x-1] == "." and grid[y][x+1] == "." and grid[y+1][x+1] == ".": return True
+        if grid[y-1][x+1] == "." and grid[y][x+1] == "." and grid[y+1][x+1] == ".": return True
         else: return False
 
 def proposeAMove(elve):
     y,x, dir = elve["y"], elve["x"], elve["moveDir"]
-    for i in range(dir, dir+4):
-        if checkAvailabilityInDir(elve, i%4):
-            elve["moveDir"] = i
+    for i in range(0, 4):
+        if checkAvailabilityInDir(elve, (dir + i)%4):
+            elve["moveDir"] = dir + i
             return True
-        elve["moveDir"] = elve["moveDir"] +1
     return False
 
 def elvesProposesMoves():
     for x in elves:
         if checkIfAnyElvesNearby(x):
-            x["wtm"] = True
-            if proposeAMove(x):x["propose"] = True
-            else: x["propose"] = False
+            x["moveDir"] = direction
+            if proposeAMove(x): x["wtm"] = True
+            else: x["wtm"] = False
         else: 
             x["wtm"] = False
             continue
@@ -48,7 +47,7 @@ def getDestCoords(elve):
 
 def checkifSpotAlreadyTaken(coords):
     if grid[coords[0]][coords[1]] != ".":
-        print("elves to same spot:", coords)
+        #print("elves to same spot:", coords)
         return True
     else: return False
 
@@ -57,10 +56,10 @@ def movePrevElveBack(coords):
     for elve in elves:
         if elve["y"] == coords[0] and elve["x"] == coords[1]:
             y,x,dir = elve["y"], elve["x"], elve["moveDir"]
-            if dir%4 == 0:  makeAMove(elve, [y+1,x] )#elve["y"] = elve["y"] +1
-            elif dir%4 == 1: elve["y"] = elve["y"] -1
-            elif dir%4 == 2: elve["x"] = elve["x"] +1
-            elif dir%4 == 3: elve["x"] = elve["x"] -1 
+            if dir%4 == 0:  makeAMove(elve, [y+1,x] )
+            elif dir%4 == 1: makeAMove(elve, [y-1,x])
+            elif dir%4 == 2: makeAMove(elve, [y,x+1])
+            elif dir%4 == 3: makeAMove(elve, [y,x-1]) 
             break
 
 def makeAMove(elve,coords):
@@ -70,15 +69,16 @@ def makeAMove(elve,coords):
 
 def elvesMove():
     for x in elves:
-        destCoords = getDestCoords(x)
-        if checkifSpotAlreadyTaken(destCoords):
-            movePrevElveBack(destCoords)
-        else:
-            makeAMove(x,destCoords)
+        if x["wtm"] == True:
+            destCoords = getDestCoords(x)
+            if checkifSpotAlreadyTaken(destCoords):
+                movePrevElveBack(destCoords)
+            else:
+                makeAMove(x,destCoords)
 
 f = open("day23/day23.txt")
 grid = []
-extra = 0 #Extra empty cells to add everywhere
+extra = 10 #Extra empty cells to add everywhere
 for x in f:
     x = "."*extra + x.rstrip() + "."*extra
     grid.append(list(x))
@@ -91,17 +91,41 @@ for i,x in enumerate(grid):
     for j,k in enumerate(x):
         if k == "#":
             cnt = cnt+1
-            elves.append({"no":cnt, "y": i,"x":j,"wtm":False,"moveDir":0})
+            elves.append({"y": i,"x":j,"wtm":False,"moveDir":3})
 
 rounds = 10
-
-for i in range(0,1):
+direction = 0
+for i in range(0,rounds):
     elvesProposesMoves()
     elvesMove()
+    direction = (direction + 1)%4
 
-for x in grid:
-    print("".join(x))
+while True:
+        if "#" not in grid[0]:
+            grid.pop(0)
+        elif "#" not in grid[len(grid)-1]:
+            grid.pop()
+        else: break
+def removeFirstRows():
+    while True:
+        for j in range(0,len(grid)):
+            if grid[j][0] == "#": return 0
+        for j in range(0,len(grid)):
+            grid[j].pop(0)
+def removeLastRows():
+    for i in range(len(grid[0])-1,0,-1):
+        for j in range(0, len(grid)):
+            if grid[j][i] == "#": return 0
+        for j in range(0,len(grid)):
+            grid[j].pop()
+removeFirstRows()
+removeLastRows()
 
-# for i in range(7,len(grid)-7):
-#     #print(x)
-#     print("".join(grid[i]))
+cnt = 0
+for i,x in enumerate(grid):
+    for k in x:
+        if k == ".": cnt = cnt +1
+print("Part1:", cnt)
+# for x in grid:
+#     print("".join(x))
+
